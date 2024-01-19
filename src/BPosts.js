45dba07f0daac380105ba4e6f43ed0e6ddef3e43
sohdonald <sohdonald@donaldUbuntu.myguest.virtualbox.org>
2024-01-19 17:36:19 -0500

    adding images

--- src/BPosts.js
+++ src/BPosts.js
@@ -3,6 +3,7 @@ import React from "react";
 function BPosts() {
     return(
         <div>
+            {/* <img src="happy fish.jpg" alt="funny image"> </img> */}
             <button>happy button</button>
             <button>silly button</button>
         </div>
