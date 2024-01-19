6917439b3e0cc0238a0a665f842f8af59dad8ca0
sohdonald <sohdonald@donaldUbuntu.myguest.virtualbox.org>
2024-01-19 17:23:03 -0500

    added buttons

---
+++ src/BPosts.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function BPosts() {
+    return(
+        <div>
+            <button>happy button</button>
+            <button>silly button</button>
+        </div>
+    );
+}
+
+export default BPosts;
--- src/Blogger.js
+++ src/Blogger.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import BPosts from "./BPosts";
 function Blogger() {
     return(
         <div>
         <h1>This is my Blog</h1>
+        <BPosts></BPosts>
         </div>
     )
 }
